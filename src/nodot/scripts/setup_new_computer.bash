fbdbdc49b9741fea41e797d6c4f4cea7f246fadc
Robin Schaufler <robin@likethebird.com>
2016-04-12 18:03:57 -0400

    Finish setting up vim

--- src/nodot/scripts/setup_new_computer.bash
+++ src/nodot/scripts/setup_new_computer.bash
@@ -47,18 +47,17 @@ curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.0/install.sh | b
 NODE_VERSION=$(curl https://nodejs.org/en/download/ 2>/dev/null | sed -n -e '/Current version:/s/.*\bv\([0-9]\+\.[0-9]\+\.[0-9]\+\)\b.*/\1/p')
 nvm install $NODE_VERSION
 
-# install Github/insanum/gcalcli
-# install Github/tpope/vim-pathogen
-# install Github/vim/bundle
-# vim: after  autoload  bundle  plugin
-# vim/after: ftplugin  syntax
-# vim/after/ftplugin: coffee.vim  python.vim
-# vim/after/syntax: css.vim  c.vim  html.vim  javascript.vim  python.vim
-# vim/plugin
-# vim/autoload/pathogen.vim
-# install the following vim bundles:
-# css.vim  vim-coffee-script  vim-fugitive  vim-less  Vundle.vim
-# install scratch plugin
-# install git plugin
+# pathogen
+mkdir -p ~/.vim/autoload ~/.vim/bundle
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+mkdir -p ~/Github/vim/bundle
+# consider tpope's vim-sensible.
+
+# vundle
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall
+vim -u NONE \
+  -c "helptags vim-fugitive/doc" \
+  -c q
 
 unset PKGINSTALL
