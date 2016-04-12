b22d26b157fbeb3f1bd3a3920ae281eb5a9be23b
Robin Schaufler <robin@likethebird.com>
2016-04-11 23:04:57 -0400

    Create distribution of both dot and no-dot files

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,6 @@
 var del    = require('del');
 var fs     = require('fs');
 var gulp   = require('gulp');
-var debug  = require('gulp-debug');
 var gzip   = require('gulp-gzip');
 var rename = require('gulp-rename');
 var tar    = require('gulp-tar');
@@ -26,7 +25,6 @@ gulp.task('clean', function () {
 gulp.task(NODOT, function () {
   var stream = gulp.src(path.join(NODOTSRC, '**/*.bash'))
       .pipe(gulp.dest(BUILD));
-  console.log(NODOT + ' return');
   return stream;
 });
 
@@ -36,18 +34,15 @@ gulp.task(DOT, function () {
         path.basename = '.' + path.basename;
       }))
       .pipe(gulp.dest(BUILD));
-  console.log(DOT + ' return');
   return stream;
 });
 
 gulp.task(BUILD, [NODOT, DOT]);
 
 gulp.task('archive', function () {
-  var stream = gulp.src(path.join(BUILD, '*'))
-      .pipe(debug({title: 'build/*'}))
+  var stream = gulp.src([path.join(BUILD, '*'), path.join(BUILD, '.*')])
       .pipe(tar('dotfiles.tar'))
       .pipe(gzip())
-      .pipe(debug({title: 'archive'}))
       .pipe(gulp.dest(DIST));
   return stream;
 });
