bd0123cc43a0c9fe10d1ca487ec6cd70f07021d6
Robin Schaufler <robin@likethebird.com>
2016-04-12 21:40:12 -0400

    Add ssh-agent to .bash_profile

---
+++ src/nodot/lib/profile/robin-Lenovo-G51-35.bash
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+# Intended to be sourced from .bash_profile
+. $HOME/lib/profile/ssh.bash
---
+++ src/nodot/lib/profile/ssh.bash
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# Intended to be sourced from .bash_profile
+
+# Start the ssh-agent.
+# It will be used by all forked child processes.
+# Before using it, make sure you have a public and private key
+# in ~/.ssh. If not, use ssh-keygen to create them.
+# Add them to the agent with ssh-add.
+# Logout and log back in again to restart the agent.
+eval $(ssh-agent -s)
