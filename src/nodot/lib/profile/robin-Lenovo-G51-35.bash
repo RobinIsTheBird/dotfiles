33544c0dd2cb92fd7cda2693fe9f8c3c21c17132
Robin Schaufler <robin@likethebird.com>
2016-04-28 08:58:13 -0400

    Add go to $PATH on ubuntu

--- src/nodot/lib/profile/robin-Lenovo-G51-35.bash
+++ src/nodot/lib/profile/robin-Lenovo-G51-35.bash
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 # Intended to be sourced from .bash_profile
+. $HOME/lib/profile/ubuntu_env.bash
 . $HOME/lib/profile/env.bash
 . $HOME/lib/profile/ssh.bash
---
+++ src/nodot/lib/profile/ubuntu_env.bash
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+# Intended to be sourced from .bash_profile
+
+PATH=/usr/local/go/bin:$PATH
