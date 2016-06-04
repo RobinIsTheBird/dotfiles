123da181274ae2f4395ac5c0625d5dfd73080a81
Robin Schaufler <robin@likethebird.com>
2016-06-04 18:25:24 -0400

    Mount syn-14 robin

--- src/nodot/lib/profile/as_sudo.bash
+++ src/nodot/lib/profile/as_sudo.bash
@@ -9,6 +9,8 @@ nc -z syn-13 111 || exit 0
 
 pgrep rpcbind &>/dev/null || sudo /sbin/rpcbind
 web="/mnt/Syn-13/web"
-robin="/mnt/Syn-13/Robin"
+robin13="/mnt/Syn-13/Robin"
+robin14="/mnt/Syn-14/Robin"
 mount | grep -s "$web" || sudo mount -t nfs "syn-13:/volume1/web" "$web"
-mount | grep -s "$robin" || sudo mount -t nfs "syn-13:/volume1/Robin" "$robin"
+mount | grep -s "$robin13" || sudo mount -t nfs "syn-13:/volume1/Robin" "$robin13"
+mount | grep -s "$robin14" ||sudo mount -t nfs "syn-14:/volume1/robin" "$robin14"
