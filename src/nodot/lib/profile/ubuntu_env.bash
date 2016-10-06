f0a4f19b879c65cb9df3c4034edbff68db87d8e2
Robin Schaufler <rschaufler@azavea.com>
2016-10-06 18:57:26 -0400

    android studio

--- src/nodot/lib/profile/ubuntu_env.bash
+++ src/nodot/lib/profile/ubuntu_env.bash
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 # Intended to be sourced from .bash_profile
 
-PATH=/usr/local/go/bin:$PATH
+[[ -d /usr/local/go/bin ]] && PATH=/usr/local/go/bin:$PATH
+[[ -d /opt/android-studio/bin ]] && PATH=/opt/android-studio/bin:$PATH
 . $HOME/lib/profile/as_sudo.bash
