766e7d4888390f3422e85a40cdde448516705f80
Robin Schaufler <robin@likethebird.com>
2016-12-11 18:32:24 -0500

    Fix operation on grep return code

--- src/nodot/scripts/new_computer/at_home/update_etc.bash
+++ src/nodot/scripts/new_computer/at_home/update_etc.bash
@@ -7,17 +7,17 @@ FOUND_CANON=$?
 CANON_IP=$([ $FOUND_CANON -eq 0 ] && echo $CANON | cut -f 1 -d ' ')
 
 ADD_HOSTS='/^$/i'
-! grep -q "127\." /etc/hosts && \
+! grep -q "127\." /etc/hosts || \
   ADD_HOSTS[${#ADD_HOSTS[@]}]="127.0.1.1 $(hostname) $DISTRO $SYSTEM"
-! grep -q "canon-pixma-5220" /etc/hosts && [ $FOUND_CANON -eq 0 ] && \
+! grep -q "canon-pixma-5220" /etc/hosts || [ $FOUND_CANON -eq 0 ] && \
   ADD_HOSTS[${#ADD_HOSTS[@]}]="${CANON_IP} canon-pixma-5220"
-! grep -q "converge" /etc/hosts && \
+! grep -q "converge" /etc/hosts || \
   ADD_HOSTS[${#ADD_HOSTS[@]}]="76.124.163.200 converge"
-! grep -q "syn-13" /etc/hosts && \
+! grep -q "syn-13" /etc/hosts || \
   ADD_HOSTS[${#ADD_HOSTS[@]}]="192.168.10.13 syn-13"
-! grep -q "syn-14" /etc/hosts && \
+! grep -q "syn-14" /etc/hosts || \
   ADD_HOSTS[${#ADD_HOSTS[@]}]="192.168.10.14 syn-14"
-! grep -q "esxi" /etc/hosts && \
+! grep -q "esxi" /etc/hosts || \
   ADD_HOSTS[${#ADD_HOSTS[@]}]="192.168.10.11 esxi"
 
 MAX=${#ADD_HOSTS[@]}
@@ -43,9 +43,9 @@ unset SED_CMD
 mkdir -p /mnt/Syn-13/web
 mkdir -p /mnt/Syn-13/Robin
 mkdir -p /mnt/Syn-14/Robin
-grep -q "Syn-13/web" /etc/fstab && \
+grep -q "Syn-13/web" /etc/fstab || \
   sudo echo 'syn-13:/volume1/web /mnt/Syn-13/web nfs rw,user,noauto 0 0' >> /etc/fstab;
-grep -q "Syn-13/Robin" /etc/fstab && \
+grep -q "Syn-13/Robin" /etc/fstab || \
   sudo echo 'syn-13:/volume1/Robin /mnt/Syn-13/Robin nfs rw,user,noauto 0 0' >> /etc/fstab;
-grep -q "Syn-14/Robin" /etc/fstab && \
+grep -q "Syn-14/Robin" /etc/fstab || \
   sudo echo 'syn-14:/volume1/robin /mnt/Syn-14/Robin nfs rw,user,noauto 0 0' >> /etc/fstab;
