7cd3069ed73ab164e54f0b92d027c9a0af61fabc
Robin Schaufler <robin@likethebird.com>
2016-12-18 17:05:43 -0500

    reconcile with lenovo

--- src/nodot/lib/profile/as_sudo.bash
+++ src/nodot/lib/profile/as_sudo.bash
@@ -1,3 +1,14 @@
 #!/usr/bin/env bash
 
+# Set by the systemdetect.bash script
+[ $AT_HOME -eq 0 ] || exit 0
+
 pgrep rpcbind &>/dev/null || sudo /sbin/rpcbind
+
+pgrep rpcbind &>/dev/null || sudo /sbin/rpcbind
+web="/mnt/Syn-13/web"
+robin13="/mnt/Syn-13/Robin"
+robin14="/mnt/Syn-14/Robin"
+mount | grep -s "$web" || sudo mount -t nfs "syn-13:/volume1/web" "$web"
+mount | grep -s "$robin13" || sudo mount -t nfs "syn-13:/volume1/Robin" "$robin13"
+mount | grep -s "$robin14" ||sudo mount -t nfs "syn-14:/volume1/robin" "$robin14"
